9dfee35e65857f7d77ab367e1aa21377f4372360
vmalyi <valentynmalyi@gmail.com>
2024-10-11 14:59:10 +0300

    change view

--- apps/number/views.py
+++ apps/number/views.py
@@ -1,12 +1,16 @@
 from django.core.handlers.wsgi import HttpRequest
 from django.http.response import JsonResponse
-from django.views.generic import DetailView
+from rest_framework.views import APIView
+
+from apps.number.serializers import NumSerializer
+from apps.number.services import PrimeService
 
 
-class PrimeView(DetailView):
-    # noinspection PyMethodOverriding
+class PrimeView(APIView):
+    # noinspection PyMethodMayBeStatic
     def get(self, request: HttpRequest, num: int):
-        for x in range(2, num):
-            if num % x == 0:
-                return JsonResponse(data={'is_prime': False})
-        return JsonResponse(data={'is_prime': True})
+        serializer = NumSerializer(data={"num": num})
+        serializer.is_valid(raise_exception=True)
+        num = serializer.validated_data["num"]
+        is_prime = PrimeService.is_prime(num)
+        return JsonResponse(data={'is_prime': is_prime})
